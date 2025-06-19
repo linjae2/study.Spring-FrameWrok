407c19aaf8387580d57ce4e046045b93bd04bbce
Lee In-Jae <injae@snudh.org>
2025-06-19 10:33:45 +0900

    Runner 관련 설정 등록

---
+++ 2025.06.19. HelloWorld/src/main/java/org/example/ApplicationRunnerImpl.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationRunnerImpl implements ApplicationRunner {
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    System.out.println("[ApplicationRunnerImpl] Hello World!");
+  }
+
+}
