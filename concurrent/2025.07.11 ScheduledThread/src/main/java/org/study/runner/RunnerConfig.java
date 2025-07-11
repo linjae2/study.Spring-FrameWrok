e5e0fafe16a10e3b90d465c70547481524a9a760
Lee In-Jae <injae@snudh.org>
2025-07-11 22:22:30 +0900

    테스트 프로그램 등록

---
+++ concurrent/2025.07.11 ScheduledThread/src/main/java/org/study/runner/RunnerConfig.java
@@ -0,0 +1,37 @@
+package org.study.runner;
+
+import java.time.LocalDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RunnerConfig {
+  final static Logger log = LoggerFactory.getLogger(RunnerConfig.class);
+
+  @Bean
+  public CommandLineRunner getTest1Runner() {
+    return args -> {
+      Thread thread = new Thread(() -> {
+        System.out.println("스레드 시작 " + LocalDateTime.now());
+        log.info("스레드 시작");
+      });
+
+      thread.start();
+      thread.join();
+    };
+  }
+
+  // @Bean
+  // public CommandLineRunner getTest2Runner() {
+  //   return args -> System.out.println("Test CommandLine Runner #2");
+  // }
+
+  // @Bean
+  // public ApplicationRunner getApp1Runner() {
+  //   return args -> System.out.println("Test Applicatin Runner #1");
+  // }
+}
