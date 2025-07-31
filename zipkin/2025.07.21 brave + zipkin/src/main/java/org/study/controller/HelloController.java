c1066f494e8032305b79a225009bf2cb63924376
Lee In-Jae <injae@snudh.org>
2025-07-31 11:31:28 +0900

    tracing 설정추가

--- zipkin/2025.07.21 brave + zipkin/src/main/java/org/study/controller/HelloController.java
+++ zipkin/2025.07.21 brave + zipkin/src/main/java/org/study/controller/HelloController.java
@@ -14,6 +14,7 @@ public class HelloController {
   @GetMapping
   public String getIndex() {
     log.info("called Hello::getIndex");
+    log.info(">> Hello::getIndex");
     return "Hello from Spring Boot!";
   }
 }
